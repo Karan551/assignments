d080741f4fedc87b824aebf839226f8a4d823a5e
Karan551 <111963317+Karan551@users.noreply.github.com>
2022-08-24 13:38:29 +0530

    Add files via upload

---
+++ assignment_1.py
@@ -0,0 +1,33 @@
+import sys
+    #Que:1 -> Write a command to get the Python version you are using.
+print('Python version is:',sys.version)
+print('Version information is:',sys.version_info)
+    #Que:-2 -> Write a python script to print "Hello Python" on the screen directly in the command line.
+print("Hello Python")
+    # Que:3 -> Write a python script to print Hello Python on the screen using VScode .
+print("Hello Python")
+    #Que:4 -> Write a python script to print Hello Python on the first line and Python on the second line.
+print("Hello \nPython")
+    #Que:5 -> Write a python script to print "MySirG" on the screen.
+print("MySirG")
+    #Que:6 -> Write a python script to print "Teacher's Day" on the screen.
+print("Teacher's Day")
+    #Que:7 -> Write a python script to print the value of a variable. Variable contains your name as data.
+name='Mahesh'
+print(name)
+    #Que:8 -> Write a python script to create a variable to store your name,age,qualification, years of experience and print it.
+name='Ram'
+age=20;x=20
+quaf='Graduation'
+exp='Fresher'
+print(name,age,quaf,exp) #this is the first method to print the variable values.
+print(f'My name is:{name}, My age is:{age}, My qualification is:{quaf}, My experience is:{exp}')# this is the second method.
+# Que:9 -> Write a python script to print values of three variables each in a new line.variables contain integer values.
+x=10
+y=20
+z=25
+print(x,'\n',y,'\n',z,sep='') # this is the first method to print the variable values.
+# this is the second method to print the variable values.
+print(x)
+print(y)
+print(z)
