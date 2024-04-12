b5d6c199f9edc6448dc3f12a43202bc2d4c2dbae
Kartik <iamkaranmishra551@gmail.com>
2024-04-13 00:31:40 +0530

    Solve Queue data structure.

--- DSA_Practice Questions/Queue Data Structure/Queue Using List/Queue_1.py
+++ DSA_Practice Questions/Queue Data Structure/Queue Using List/Queue_1.py
@@ -9,14 +9,22 @@ class Queue:
         self.my_queue.append(data)
 
     def dequeue(self):
-        self.my_queue.pop(0)
+        if not self.is_empty():
+            self.my_queue.pop(0)
+        else:
+            IndexError("Queue Under Flow.")
 
     def get_front(self):
-        return self.my_queue[0]
+        if not self.is_empty():
+            return self.my_queue[0]
+        else:
+            IndexError("Queue Under Flow.")
 
     def get_rear(self):
-        return self.my_queue[-1]
-        pass
+        if not self.is_empty():
+            return self.my_queue[-1]
+        else:
+            IndexError("Queue Under Flow.")
 
     def size(self):
         return len(self.my_queue)
@@ -33,9 +41,16 @@ print("This is the oldest element of the Queue:", myQueue.get_front())
 print("This is the Latest element of the Queue:", myQueue.get_rear())
 myQueue.dequeue()
 myQueue.dequeue()
-myQueue.enqueue(60)
+myQueue.dequeue()
+myQueue.dequeue()
+# myQueue.enqueue(60)
 print("---------------------------")
-print("This is the oldest element of the Queue:", myQueue.get_front())
-print("This is the Latest element of the Queue:", myQueue.get_rear())
-print("Queue is empty or not:", myQueue.is_empty())
-print("size of the queue:",myQueue.size())
+# print("This is the oldest element of the Queue:", myQueue.get_front())
+# print("This is the Latest element of the Queue:", myQueue.get_rear())
+# print("Queue is empty or not:", myQueue.is_empty())
+# print("size of the queue:", myQueue.size())
+# To Handle Exception
+try:
+    print(myQueue.get_front())
+except Exception as e:
+    print(e.args[0])
